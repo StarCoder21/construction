e7315c17244a56feb90967567a2a633a67cf11ec
StarCoder21 <Paarth608@gmail.com>
2024-12-31 11:57:55 +0530

    Add files via upload

---
+++ constructer.py
@@ -0,0 +1,14 @@
+class IOString():
+    
+    def __init__(self):
+        self.str1 =""
+
+    def get_string(self):
+        self.str1 = input("enter word: ")
+
+    def print_string(self):
+        print(" result is: ", self.str1.upper())
+
+str1=IOString()
+str1.get_string()
+str1.print_string()
